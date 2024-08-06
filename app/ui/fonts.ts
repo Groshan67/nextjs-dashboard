b66ade8a13ce1f03ce5ed5014d7de2d2f4fd0994
Groshan67 <ghassemroshan@gmail.com>
2024-08-06 15:37:36 +0330

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { JetBrains_Mono,Noto_Kufi_Arabic,Lusitana } from 'next/font/google';
+
+export const jetbrain = JetBrains_Mono({ subsets: ['latin'], weight: ['300', '500'] })
+export const noto = Noto_Kufi_Arabic({ subsets: ['latin'], weight: ['300', '500'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
