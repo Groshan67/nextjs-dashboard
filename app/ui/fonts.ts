26667f83395051a0ef20031d9b1560c1c8b34a61
Groshan67 <ghassemroshan@gmail.com>
2024-08-07 10:08:57 +0330

    delete

--- app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { JetBrains_Mono,Noto_Kufi_Arabic,Lusitana } from 'next/font/google';
-
-export const jetbrain = JetBrains_Mono({ subsets: ['latin'], weight: ['300', '500'] })
-export const noto = Noto_Kufi_Arabic({ subsets: ['latin'], weight: ['300', '500'] })
-export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
