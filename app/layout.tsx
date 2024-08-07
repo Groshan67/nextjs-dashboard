7e72fa580e8a0fcf9c41e11ca8d76903a2384d15
Groshan67 <ghassemroshan@gmail.com>
2024-08-07 15:28:49 +0330

    shol2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css';
+import { jetbrains } from './ui/font';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${jetbrains.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/font.ts
@@ -0,0 +1,5 @@
+import { JetBrains_Mono, Noto_Kufi_Arabic, Lusitana } from 'next/font/google';
+
+export const jetbrains = JetBrains_Mono({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400'] });
+export const noto = Noto_Kufi_Arabic({ subsets: ['arabic'] });
