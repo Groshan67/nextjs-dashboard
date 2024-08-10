083c13d43c4281c4fed6b181f694a3a407bd2db3
Groshan67 <ghassemroshan@gmail.com>
2024-08-10 15:11:18 +0330

    start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
-import '@/app/ui/global.css';
-import { jetbrains } from './ui/fonts';
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={`${jetbrains.className} antialiased`}>{children}</body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
