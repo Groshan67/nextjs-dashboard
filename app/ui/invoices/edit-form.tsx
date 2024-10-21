5ce2fe12d1e7f0836aa42f3b0857bb5fed808056
Groshan67 <ghassemroshan@gmail.com>
2024-10-21 15:57:34 +0330

    resolve error2

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -72,7 +72,7 @@ export default function EditInvoiceForm({
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
             <div id="amount-error">
-              {state.errors?.amount && state.errors}
+              {state.errors?.amount}
             </div>
           </div>
         </div>
