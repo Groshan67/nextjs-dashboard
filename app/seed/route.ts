607f50a598af87b57a39814fc9e9ede68519eca5
Groshan67 <ghassemroshan@gmail.com>
2024-10-21 15:45:14 +0330

    resolve error2

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -104,6 +104,6 @@
 export async function GET() {
   return Response.json({
     message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+      'Uncomment this file and remove this line. You can delete this file when you are finishedS.',
   });
 }
